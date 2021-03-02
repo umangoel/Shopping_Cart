0d2c5720182dba38534dc2987da2ae8c9c616d37
Umang Goel <umang@Umangs-MacBook-Air.local>
2021-03-02 09:55:45 +0530

    Creating Cart-Item

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,7 +11,12 @@ class CartItem extends React.Component{
                 <div style={{fontSize: 25}}>Title</div>
                 <div style={{color: 'grey'}}>Rs. 999</div>
                 <div style={{color: 'blue'}}>Qty: 1</div>
-                <div className="action-icons">
+                <div className="cart-item-actions">
+                    <div>
+                        <img alt="increase" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/149/149145.svg?token=exp=1614611575~hmac=d30739dda24c96cb260cc29fd6657179"/>
+                        <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/149/149146.svg?token=exp=1614613401~hmac=e2130936326f3d0843d61604f4c5acfc"/> 
+                        <img alt="remove" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/1214/1214428.svg?token=exp=1614613461~hmac=6f52cdedccccde9114bbf0f03714b9e1"/> 
+                    </div>
 
                 </div>
             </div>
@@ -26,6 +31,7 @@ const styles = {
         width: 100,
         borderRadius: 5,
     }
+    
 };
 
 export default CartItem;
